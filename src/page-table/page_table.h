d2178fb40fb2e791c77214b358ed26b44b8450a2
Ivan Baturkin <ivanmysterys@gmail.com>
2022-12-16 15:58:20 +0200

    feat: add page table definition

--- src/page-table/page_table.h
+++ src/page-table/page_table.h
@@ -1,7 +1,13 @@
-#include <page_table_entry.h>
+#ifndef PAGE_TABLE_H
+#define PAGE_TABLE_H
+
+#include "page_table_entry.h"
 
 typedef struct PageTable PageTable;
 struct PageTable
 {
     PageTableEntry** pageEntries;
 };
+
+
+#endif // PAGE_TABLE_H

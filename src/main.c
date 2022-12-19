f83d13476d8574521b487d824635007c6b21bf2f
Ivan Baturkin <ivanmysterys@gmail.com>
2022-12-19 14:22:39 +0200

    feat: add basic process usage

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <time.h>
+#include "process/process.h"
+
 
 int main()
 {
-    printf("Hello world!\n");
+    srand(time(0));
+
+    printf("sizeof(Process) = %ld\n", sizeof(Process));
+    printf("Before allocation\n");
+
+    Process* p = Process_initRandomized();
+    printf("After allocation\n");
+    Process_free(p);
+
+    printf("After freeing\n");
+
     return 0;
 }

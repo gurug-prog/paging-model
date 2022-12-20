4dd220ea3811c0c581ecae808b1882bb5ff621ff
Ivan Baturkin <ivanmysterys@gmail.com>
2022-12-20 18:48:47 +0200

    feat: use uniq index generation

--- src/process/process_generator.c
+++ src/process/process_generator.c
@@ -16,14 +16,15 @@ WorkingSet* ProcessGenerator_genWorkingSet(PageTable* pageTable)
 {
     size_t wsSize = GEN_VALUE(PROCESS_MIN_VAS_SIZE, pageTable->size);
     WorkingSet* ws = WorkingSet_initSize(wsSize);
+    Random* rand = Random_init();
 
     for (size_t i = 0; i < wsSize; i++)
     {
-        // TODO: generate uniq values
-        int randIndex = GEN_VALUE(0, pageTable->size);
+        int randIndex = Random_genUniqInt(rand, 0, pageTable->size - 1);
         PageTableEntry* page = pageTable->pageEntries[randIndex];
         WorkingSet_setPage(ws, page, i);
     }
+    Random_free(rand);
 
     return ws;
 }
